b3c5c77f24e253ea47901ede867c76d834c6812a
javierpelaez97 <javierpelaezgimenez@gmail.com>
2025-07-31 17:02:58 +0200

    first commit

---
+++ 01-tic-tac-toe/src/App.jsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+
+const TURNS = {
+  X: "X",
+  O: "O",
+};
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? "is-selected" : " "} `;
+
+  const handleClick = () => {
+    updateBoard(index);
+  };
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  );
+};
+
+const WINNER_COMBOS = [
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  const [turn, setTurn] = useState(TURNS.X); // Es un hook que determina un estado al inicio de la aplicacion. Crea un array de dos posiciones
+
+  const [winner, setWinner] = useState(null);
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a];
+      }
+    }
+    return null;
+  };
+
+  const updateBoard = (index) => {
+    if (board[index] || winner) return;       // Inidicamos que si el board tiene algo no lo pintamos ni hacemos nada de nuevo
+    /* Creamos un nuevo tablero recogemos el inidce de la posicion y le indicamos el turno que estamos */
+    const newBoard = [...board];              // Para crear una copia de un array de forma superficial
+    newBoard[index] = turn;                   //Debemos tratar los estados como inmutables
+    setBoard(newBoard);
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(()=>{
+        return newWinner
+      })
+      alert(`Ela ganador es ${winner}`)
+      
+    }
+  };
+
+  return (
+    <main className="board">
+      <h1>Tic tac toe</h1>
+      <section className="game">
+        {board.map((_, index) => {
+          return (
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {board[index]}
+            </Square>
+          );
+        })}
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+    </main>
+  );
+}
+
+export default App;
